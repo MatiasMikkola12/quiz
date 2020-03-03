3057502e904ab62e6e184f7e9f278a9ac6ee5d78
Matias Mikkola <matias.mikkola@sainsburys.co.uk>
2020-03-03 11:45:16 +0000

    Renaming stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
         this.state = {
             questions: [],
-            fields: {},
+            responses: {},
             errors: {},
         }
 
@@ -22,10 +22,10 @@ class App extends Component {
             .then(data => this.setState({ questions: data }));
     }
 
-    handleChange(field, e) {
-        let fields = this.state.fields;
-        fields[field] = e.target.value;
-        this.setState({ fields });
+    handleChange(response, e) {
+        let responses = this.state.responses;
+        responses[response] = e.target.value;
+        this.setState({ responses });
     }
 
     handleSubmit(e) {
@@ -36,18 +36,18 @@ class App extends Component {
     }
 
     handleValidation(answers) {
-        let fields = this.state.fields;
+        let responses = this.state.responses;
         let errors = {};
         
-        if (fields["1"] != answers.answers[0].correct) {
+        if (responses["1"] != answers.answers[0].correct) {
             errors["1"] = "WRONG";
         }
 
-        if (fields["2"] != answers.answers[1].correct) {
+        if (responses["2"] != answers.answers[1].correct) {
             errors["2"] = "WRONG";
         }
 
-        if (fields["3"] != answers.answers[2].correct) {
+        if (responses["3"] != answers.answers[2].correct) {
             errors["3"] = "WRONG";
         }
 
@@ -59,17 +59,17 @@ class App extends Component {
         const questions = this.state.questions.questions
         return (
             <div>
-                <form name="contactform" className="contactform" onSubmit={this.handleSubmit}>
+                <form name="quiz" className="quiz" onSubmit={this.handleSubmit}>
                     {
                         questions && questions.map(question => (
                             <div>
                                 <h3>{question.question}</h3>
 
                                 <input name={question.id} ref={question.id} type="radio" size="30" onChange={this.handleChange.bind(this, `${question.id}`)} value={question.option} />
-                                <label for="contactChoice1">{question.option}</label>
+                                <label for="choiceOne">{question.option}</label>
 
                                 <input name={question.id} ref={question.id} type="radio" size="30" onChange={this.handleChange.bind(this, `${question.id}`)} value={question.option2} />
-                                <label for="contactChoice1">{question.option2}</label>
+                                <label for="choiceTwo">{question.option2}</label>
                                 <span className="error">{this.state.errors[`${question.id}`]}</span>
                             </div>
                         ))
